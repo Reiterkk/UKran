6047e66e0d6b939d95c78651744f53b67effcf52
Kevin Reiter <reiter.kk@gmail.com>
2020-09-01 18:09:05 +0200

    Add project files.

---
+++ UKran/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace UKran
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdHakenAus = new System.Windows.Forms.Button();
+            this.CmdHakenEin = new System.Windows.Forms.Button();
+            this.CmdAuslegerAus = new System.Windows.Forms.Button();
+            this.CmdAuslegerEin = new System.Windows.Forms.Button();
+            this.CmdKranRechts = new System.Windows.Forms.Button();
+            this.CmdKranLinks = new System.Windows.Forms.Button();
+            this.CmdKranAus = new System.Windows.Forms.Button();
+            this.CmdKranEin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CmdHakenAus
+            // 
+            this.CmdHakenAus.Location = new System.Drawing.Point(698, 53);
+            this.CmdHakenAus.Name = "CmdHakenAus";
+            this.CmdHakenAus.Size = new System.Drawing.Size(86, 23);
+            this.CmdHakenAus.TabIndex = 0;
+            this.CmdHakenAus.Text = "Haken aus";
+            this.CmdHakenAus.UseVisualStyleBackColor = true;
+            // 
+            // CmdHakenEin
+            // 
+            this.CmdHakenEin.Location = new System.Drawing.Point(698, 99);
+            this.CmdHakenEin.Name = "CmdHakenEin";
+            this.CmdHakenEin.Size = new System.Drawing.Size(86, 23);
+            this.CmdHakenEin.TabIndex = 1;
+            this.CmdHakenEin.Text = "Haken ein";
+            this.CmdHakenEin.UseVisualStyleBackColor = true;
+            // 
+            // CmdAuslegerAus
+            // 
+            this.CmdAuslegerAus.Location = new System.Drawing.Point(698, 145);
+            this.CmdAuslegerAus.Name = "CmdAuslegerAus";
+            this.CmdAuslegerAus.Size = new System.Drawing.Size(86, 23);
+            this.CmdAuslegerAus.TabIndex = 2;
+            this.CmdAuslegerAus.Text = "Ausleger aus";
+            this.CmdAuslegerAus.UseVisualStyleBackColor = true;
+            // 
+            // CmdAuslegerEin
+            // 
+            this.CmdAuslegerEin.Location = new System.Drawing.Point(698, 191);
+            this.CmdAuslegerEin.Name = "CmdAuslegerEin";
+            this.CmdAuslegerEin.Size = new System.Drawing.Size(86, 23);
+            this.CmdAuslegerEin.TabIndex = 3;
+            this.CmdAuslegerEin.Text = "Ausleger ein";
+            this.CmdAuslegerEin.UseVisualStyleBackColor = true;
+            // 
+            // CmdKranRechts
+            // 
+            this.CmdKranRechts.Location = new System.Drawing.Point(698, 237);
+            this.CmdKranRechts.Name = "CmdKranRechts";
+            this.CmdKranRechts.Size = new System.Drawing.Size(86, 23);
+            this.CmdKranRechts.TabIndex = 4;
+            this.CmdKranRechts.Text = "Kran rechts";
+            this.CmdKranRechts.UseVisualStyleBackColor = true;
+            // 
+            // CmdKranLinks
+            // 
+            this.CmdKranLinks.Location = new System.Drawing.Point(698, 283);
+            this.CmdKranLinks.Name = "CmdKranLinks";
+            this.CmdKranLinks.Size = new System.Drawing.Size(86, 23);
+            this.CmdKranLinks.TabIndex = 5;
+            this.CmdKranLinks.Text = "Kran links";
+            this.CmdKranLinks.UseVisualStyleBackColor = true;
+            // 
+            // CmdKranAus
+            // 
+            this.CmdKranAus.Location = new System.Drawing.Point(698, 329);
+            this.CmdKranAus.Name = "CmdKranAus";
+            this.CmdKranAus.Size = new System.Drawing.Size(86, 23);
+            this.CmdKranAus.TabIndex = 6;
+            this.CmdKranAus.Text = "Kran aus";
+            this.CmdKranAus.UseVisualStyleBackColor = true;
+            // 
+            // CmdKranEin
+            // 
+            this.CmdKranEin.Location = new System.Drawing.Point(698, 375);
+            this.CmdKranEin.Name = "CmdKranEin";
+            this.CmdKranEin.Size = new System.Drawing.Size(86, 23);
+            this.CmdKranEin.TabIndex = 7;
+            this.CmdKranEin.Text = "Kran ein";
+            this.CmdKranEin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 450);
+            this.Controls.Add(this.CmdKranEin);
+            this.Controls.Add(this.CmdKranAus);
+            this.Controls.Add(this.CmdKranLinks);
+            this.Controls.Add(this.CmdKranRechts);
+            this.Controls.Add(this.CmdAuslegerEin);
+            this.Controls.Add(this.CmdAuslegerAus);
+            this.Controls.Add(this.CmdHakenEin);
+            this.Controls.Add(this.CmdHakenAus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdHakenAus;
+        private System.Windows.Forms.Button CmdHakenEin;
+        private System.Windows.Forms.Button CmdAuslegerAus;
+        private System.Windows.Forms.Button CmdAuslegerEin;
+        private System.Windows.Forms.Button CmdKranRechts;
+        private System.Windows.Forms.Button CmdKranLinks;
+        private System.Windows.Forms.Button CmdKranAus;
+        private System.Windows.Forms.Button CmdKranEin;
+    }
+}
+
