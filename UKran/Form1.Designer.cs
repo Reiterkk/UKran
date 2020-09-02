066a5b5c1ca3b89cc01d2b1439397332822c0aab
Kevin Reiter <kevin.reiter@kit.edu>
2020-09-02 16:36:19 +0200

    Bewegung nach links und rechts sowie das Aus- und Einfahren des Hauptelements implementiert.

--- UKran/Form1.Designer.cs
+++ UKran/Form1.Designer.cs
@@ -44,9 +44,10 @@
             // 
             // CmdHakenAus
             // 
-            this.CmdHakenAus.Location = new System.Drawing.Point(698, 53);
+            this.CmdHakenAus.Location = new System.Drawing.Point(1396, 102);
+            this.CmdHakenAus.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.CmdHakenAus.Name = "CmdHakenAus";
-            this.CmdHakenAus.Size = new System.Drawing.Size(86, 23);
+            this.CmdHakenAus.Size = new System.Drawing.Size(172, 44);
             this.CmdHakenAus.TabIndex = 0;
             this.CmdHakenAus.Text = "Haken aus";
             this.CmdHakenAus.UseVisualStyleBackColor = true;
@@ -54,9 +55,10 @@
             // 
             // CmdHakenEin
             // 
-            this.CmdHakenEin.Location = new System.Drawing.Point(698, 99);
+            this.CmdHakenEin.Location = new System.Drawing.Point(1396, 190);
+            this.CmdHakenEin.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.CmdHakenEin.Name = "CmdHakenEin";
-            this.CmdHakenEin.Size = new System.Drawing.Size(86, 23);
+            this.CmdHakenEin.Size = new System.Drawing.Size(172, 44);
             this.CmdHakenEin.TabIndex = 1;
             this.CmdHakenEin.Text = "Haken ein";
             this.CmdHakenEin.UseVisualStyleBackColor = true;
@@ -64,9 +66,10 @@
             // 
             // CmdAuslegerAus
             // 
-            this.CmdAuslegerAus.Location = new System.Drawing.Point(698, 145);
+            this.CmdAuslegerAus.Location = new System.Drawing.Point(1396, 279);
+            this.CmdAuslegerAus.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.CmdAuslegerAus.Name = "CmdAuslegerAus";
-            this.CmdAuslegerAus.Size = new System.Drawing.Size(86, 23);
+            this.CmdAuslegerAus.Size = new System.Drawing.Size(172, 44);
             this.CmdAuslegerAus.TabIndex = 2;
             this.CmdAuslegerAus.Text = "Ausleger aus";
             this.CmdAuslegerAus.UseVisualStyleBackColor = true;
@@ -74,9 +77,10 @@
             // 
             // CmdAuslegerEin
             // 
-            this.CmdAuslegerEin.Location = new System.Drawing.Point(698, 191);
+            this.CmdAuslegerEin.Location = new System.Drawing.Point(1396, 367);
+            this.CmdAuslegerEin.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.CmdAuslegerEin.Name = "CmdAuslegerEin";
-            this.CmdAuslegerEin.Size = new System.Drawing.Size(86, 23);
+            this.CmdAuslegerEin.Size = new System.Drawing.Size(172, 44);
             this.CmdAuslegerEin.TabIndex = 3;
             this.CmdAuslegerEin.Text = "Ausleger ein";
             this.CmdAuslegerEin.UseVisualStyleBackColor = true;
@@ -84,77 +88,89 @@
             // 
             // CmdKranRechts
             // 
-            this.CmdKranRechts.Location = new System.Drawing.Point(698, 237);
+            this.CmdKranRechts.Location = new System.Drawing.Point(1396, 456);
+            this.CmdKranRechts.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.CmdKranRechts.Name = "CmdKranRechts";
-            this.CmdKranRechts.Size = new System.Drawing.Size(86, 23);
+            this.CmdKranRechts.Size = new System.Drawing.Size(172, 44);
             this.CmdKranRechts.TabIndex = 4;
             this.CmdKranRechts.Text = "Kran rechts";
             this.CmdKranRechts.UseVisualStyleBackColor = true;
+            this.CmdKranRechts.Click += new System.EventHandler(this.CmdKranRechts_Click);
             // 
             // CmdKranLinks
             // 
-            this.CmdKranLinks.Location = new System.Drawing.Point(698, 283);
+            this.CmdKranLinks.Location = new System.Drawing.Point(1396, 544);
+            this.CmdKranLinks.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.CmdKranLinks.Name = "CmdKranLinks";
-            this.CmdKranLinks.Size = new System.Drawing.Size(86, 23);
+            this.CmdKranLinks.Size = new System.Drawing.Size(172, 44);
             this.CmdKranLinks.TabIndex = 5;
             this.CmdKranLinks.Text = "Kran links";
             this.CmdKranLinks.UseVisualStyleBackColor = true;
+            this.CmdKranLinks.Click += new System.EventHandler(this.CmdKranLinks_Click);
             // 
             // CmdKranAus
             // 
-            this.CmdKranAus.Location = new System.Drawing.Point(698, 329);
+            this.CmdKranAus.Location = new System.Drawing.Point(1396, 633);
+            this.CmdKranAus.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.CmdKranAus.Name = "CmdKranAus";
-            this.CmdKranAus.Size = new System.Drawing.Size(86, 23);
+            this.CmdKranAus.Size = new System.Drawing.Size(172, 44);
             this.CmdKranAus.TabIndex = 6;
             this.CmdKranAus.Text = "Kran aus";
             this.CmdKranAus.UseVisualStyleBackColor = true;
+            this.CmdKranAus.Click += new System.EventHandler(this.CmdKranAus_Click);
             // 
             // CmdKranEin
             // 
-            this.CmdKranEin.Location = new System.Drawing.Point(698, 375);
+            this.CmdKranEin.Location = new System.Drawing.Point(1396, 721);
+            this.CmdKranEin.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.CmdKranEin.Name = "CmdKranEin";
-            this.CmdKranEin.Size = new System.Drawing.Size(86, 23);
+            this.CmdKranEin.Size = new System.Drawing.Size(172, 44);
             this.CmdKranEin.TabIndex = 7;
             this.CmdKranEin.Text = "Kran ein";
             this.CmdKranEin.UseVisualStyleBackColor = true;
+            this.CmdKranEin.Click += new System.EventHandler(this.CmdKranEin_Click);
             // 
             // PnlHaken
             // 
             this.PnlHaken.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.PnlHaken.Location = new System.Drawing.Point(405, 300);
+            this.PnlHaken.Location = new System.Drawing.Point(810, 577);
+            this.PnlHaken.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.PnlHaken.Name = "PnlHaken";
-            this.PnlHaken.Size = new System.Drawing.Size(10, 20);
+            this.PnlHaken.Size = new System.Drawing.Size(20, 38);
             this.PnlHaken.TabIndex = 8;
             // 
             // PnlAusleger
             // 
             this.PnlAusleger.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.PnlAusleger.Location = new System.Drawing.Point(405, 290);
+            this.PnlAusleger.Location = new System.Drawing.Point(810, 558);
+            this.PnlAusleger.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.PnlAusleger.Name = "PnlAusleger";
-            this.PnlAusleger.Size = new System.Drawing.Size(80, 10);
+            this.PnlAusleger.Size = new System.Drawing.Size(160, 19);
             this.PnlAusleger.TabIndex = 0;
             // 
             // PnlHauptelement
             // 
             this.PnlHauptelement.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.PnlHauptelement.Location = new System.Drawing.Point(475, 300);
+            this.PnlHauptelement.Location = new System.Drawing.Point(950, 577);
+            this.PnlHauptelement.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.PnlHauptelement.Name = "PnlHauptelement";
-            this.PnlHauptelement.Size = new System.Drawing.Size(10, 100);
+            this.PnlHauptelement.Size = new System.Drawing.Size(20, 192);
             this.PnlHauptelement.TabIndex = 0;
             // 
             // PnlFundament
             // 
             this.PnlFundament.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.PnlFundament.Location = new System.Drawing.Point(450, 400);
+            this.PnlFundament.Location = new System.Drawing.Point(900, 769);
+            this.PnlFundament.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.PnlFundament.Name = "PnlFundament";
-            this.PnlFundament.Size = new System.Drawing.Size(60, 10);
+            this.PnlFundament.Size = new System.Drawing.Size(120, 19);
             this.PnlFundament.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(811, 450);
+            this.ClientSize = new System.Drawing.Size(1622, 865);
             this.Controls.Add(this.PnlAusleger);
             this.Controls.Add(this.PnlHauptelement);
             this.Controls.Add(this.PnlFundament);
@@ -167,6 +183,7 @@
             this.Controls.Add(this.CmdAuslegerAus);
             this.Controls.Add(this.CmdHakenEin);
             this.Controls.Add(this.CmdHakenAus);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- UKran/Form1.cs
+++ UKran/Form1.cs
@@ -45,5 +45,40 @@ namespace UKran
             }
             
         }
+
+        private void CmdKranRechts_Click(object sender, EventArgs e)
+        {
+            PnlAusleger.Location = new Point(PnlAusleger.Location.X + 10, PnlAusleger.Location.Y);
+            PnlHaken.Location = new Point(PnlHaken.Location.X + 10, PnlHaken.Location.Y);
+            PnlFundament.Location = new Point(PnlFundament.Location.X + 10, PnlFundament.Location.Y);
+            PnlHauptelement.Location = new Point(PnlHauptelement.Location.X + 10, PnlHauptelement.Location.Y);
+        }
+
+        private void CmdKranLinks_Click(object sender, EventArgs e)
+        {
+            PnlAusleger.Location = new Point(PnlAusleger.Location.X - 10, PnlAusleger.Location.Y);
+            PnlHaken.Location = new Point(PnlHaken.Location.X - 10, PnlHaken.Location.Y);
+            PnlFundament.Location = new Point(PnlFundament.Location.X - 10, PnlFundament.Location.Y);
+            PnlHauptelement.Location = new Point(PnlHauptelement.Location.X - 10, PnlHauptelement.Location.Y);
+        }
+
+        private void CmdKranAus_Click(object sender, EventArgs e)
+        {
+            PnlAusleger.Location = new Point(PnlAusleger.Location.X, PnlAusleger.Location.Y -10);
+            PnlHaken.Location = new Point(PnlHaken.Location.X, PnlHaken.Location.Y -10);
+            PnlHauptelement.Location = new Point(PnlHauptelement.Location.X, PnlHauptelement.Location.Y -10);
+            PnlHauptelement.Height += 10;
+        }
+
+        private void CmdKranEin_Click(object sender, EventArgs e)
+        {
+            if (PnlHauptelement.Height > 10)
+            {
+                PnlAusleger.Location = new Point(PnlAusleger.Location.X, PnlAusleger.Location.Y + 10);
+                PnlHaken.Location = new Point(PnlHaken.Location.X, PnlHaken.Location.Y + 10);
+                PnlHauptelement.Location = new Point(PnlHauptelement.Location.X, PnlHauptelement.Location.Y + 10);
+                PnlHauptelement.Height -= 10;
+            }
+        }
     }
 }
