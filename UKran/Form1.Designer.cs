b550829f143c67292bb9dd8a2d01fa2056011cee
Kevin Reiter <kevin.reiter@kit.edu>
2020-09-04 17:51:13 +0200

    RadioButtons als Optionen sowie einen Timer hinzugefügt. Bewegung lässt sich über Start und Stop Button starten bzw. beenden.

--- UKran/Form1.Designer.cs
+++ UKran/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.CmdHakenAus = new System.Windows.Forms.Button();
             this.CmdHakenEin = new System.Windows.Forms.Button();
             this.CmdAuslegerAus = new System.Windows.Forms.Button();
@@ -40,6 +41,17 @@
             this.PnlAusleger = new System.Windows.Forms.Panel();
             this.PnlHauptelement = new System.Windows.Forms.Panel();
             this.PnlFundament = new System.Windows.Forms.Panel();
+            this.rBtnHakenAus = new System.Windows.Forms.RadioButton();
+            this.rBtnHakenEin = new System.Windows.Forms.RadioButton();
+            this.rBtnAuslegerAus = new System.Windows.Forms.RadioButton();
+            this.rBtnAuslegerEin = new System.Windows.Forms.RadioButton();
+            this.rBtnKranRechts = new System.Windows.Forms.RadioButton();
+            this.rBtnKranLinks = new System.Windows.Forms.RadioButton();
+            this.rBtnKranAus = new System.Windows.Forms.RadioButton();
+            this.rBtnKranEin = new System.Windows.Forms.RadioButton();
+            this.CmdStart = new System.Windows.Forms.Button();
+            this.CmdStop = new System.Windows.Forms.Button();
+            this.TimKran = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // CmdHakenAus
@@ -166,11 +178,135 @@
             this.PnlFundament.Size = new System.Drawing.Size(120, 19);
             this.PnlFundament.TabIndex = 0;
             // 
+            // rBtnHakenAus
+            // 
+            this.rBtnHakenAus.AutoSize = true;
+            this.rBtnHakenAus.Checked = true;
+            this.rBtnHakenAus.Location = new System.Drawing.Point(1131, 141);
+            this.rBtnHakenAus.Name = "rBtnHakenAus";
+            this.rBtnHakenAus.Size = new System.Drawing.Size(146, 29);
+            this.rBtnHakenAus.TabIndex = 9;
+            this.rBtnHakenAus.TabStop = true;
+            this.rBtnHakenAus.Text = "Haken aus";
+            this.rBtnHakenAus.UseVisualStyleBackColor = true;
+            this.rBtnHakenAus.CheckedChanged += new System.EventHandler(this.rBtnHakenAus_CheckedChanged);
+            // 
+            // rBtnHakenEin
+            // 
+            this.rBtnHakenEin.AutoSize = true;
+            this.rBtnHakenEin.Location = new System.Drawing.Point(1131, 229);
+            this.rBtnHakenEin.Name = "rBtnHakenEin";
+            this.rBtnHakenEin.Size = new System.Drawing.Size(140, 29);
+            this.rBtnHakenEin.TabIndex = 10;
+            this.rBtnHakenEin.Text = "Haken ein";
+            this.rBtnHakenEin.UseVisualStyleBackColor = true;
+            // 
+            // rBtnAuslegerAus
+            // 
+            this.rBtnAuslegerAus.AutoSize = true;
+            this.rBtnAuslegerAus.Location = new System.Drawing.Point(1131, 318);
+            this.rBtnAuslegerAus.Name = "rBtnAuslegerAus";
+            this.rBtnAuslegerAus.Size = new System.Drawing.Size(169, 29);
+            this.rBtnAuslegerAus.TabIndex = 11;
+            this.rBtnAuslegerAus.TabStop = true;
+            this.rBtnAuslegerAus.Text = "Ausleger aus";
+            this.rBtnAuslegerAus.UseVisualStyleBackColor = true;
+            // 
+            // rBtnAuslegerEin
+            // 
+            this.rBtnAuslegerEin.AutoSize = true;
+            this.rBtnAuslegerEin.Location = new System.Drawing.Point(1131, 406);
+            this.rBtnAuslegerEin.Name = "rBtnAuslegerEin";
+            this.rBtnAuslegerEin.Size = new System.Drawing.Size(163, 29);
+            this.rBtnAuslegerEin.TabIndex = 12;
+            this.rBtnAuslegerEin.TabStop = true;
+            this.rBtnAuslegerEin.Text = "Ausleger ein";
+            this.rBtnAuslegerEin.UseVisualStyleBackColor = true;
+            // 
+            // rBtnKranRechts
+            // 
+            this.rBtnKranRechts.AutoSize = true;
+            this.rBtnKranRechts.Location = new System.Drawing.Point(1131, 495);
+            this.rBtnKranRechts.Name = "rBtnKranRechts";
+            this.rBtnKranRechts.Size = new System.Drawing.Size(153, 29);
+            this.rBtnKranRechts.TabIndex = 13;
+            this.rBtnKranRechts.TabStop = true;
+            this.rBtnKranRechts.Text = "Kran rechts";
+            this.rBtnKranRechts.UseVisualStyleBackColor = true;
+            // 
+            // rBtnKranLinks
+            // 
+            this.rBtnKranLinks.AutoSize = true;
+            this.rBtnKranLinks.Location = new System.Drawing.Point(1131, 583);
+            this.rBtnKranLinks.Name = "rBtnKranLinks";
+            this.rBtnKranLinks.Size = new System.Drawing.Size(138, 29);
+            this.rBtnKranLinks.TabIndex = 14;
+            this.rBtnKranLinks.TabStop = true;
+            this.rBtnKranLinks.Text = "Kran links";
+            this.rBtnKranLinks.UseVisualStyleBackColor = true;
+            // 
+            // rBtnKranAus
+            // 
+            this.rBtnKranAus.AutoSize = true;
+            this.rBtnKranAus.Location = new System.Drawing.Point(1131, 672);
+            this.rBtnKranAus.Name = "rBtnKranAus";
+            this.rBtnKranAus.Size = new System.Drawing.Size(129, 29);
+            this.rBtnKranAus.TabIndex = 15;
+            this.rBtnKranAus.TabStop = true;
+            this.rBtnKranAus.Text = "Kran aus";
+            this.rBtnKranAus.UseVisualStyleBackColor = true;
+            // 
+            // rBtnKranEin
+            // 
+            this.rBtnKranEin.AutoSize = true;
+            this.rBtnKranEin.Location = new System.Drawing.Point(1131, 760);
+            this.rBtnKranEin.Name = "rBtnKranEin";
+            this.rBtnKranEin.Size = new System.Drawing.Size(123, 29);
+            this.rBtnKranEin.TabIndex = 16;
+            this.rBtnKranEin.TabStop = true;
+            this.rBtnKranEin.Text = "Kran ein";
+            this.rBtnKranEin.UseVisualStyleBackColor = true;
+            // 
+            // CmdStart
+            // 
+            this.CmdStart.Location = new System.Drawing.Point(1329, 846);
+            this.CmdStart.Name = "CmdStart";
+            this.CmdStart.Size = new System.Drawing.Size(172, 44);
+            this.CmdStart.TabIndex = 17;
+            this.CmdStart.Text = "Start";
+            this.CmdStart.UseVisualStyleBackColor = true;
+            this.CmdStart.Click += new System.EventHandler(this.CmdStart_Click);
+            // 
+            // CmdStop
+            // 
+            this.CmdStop.Location = new System.Drawing.Point(1329, 913);
+            this.CmdStop.Name = "CmdStop";
+            this.CmdStop.Size = new System.Drawing.Size(172, 44);
+            this.CmdStop.TabIndex = 18;
+            this.CmdStop.Text = "Stop";
+            this.CmdStop.UseVisualStyleBackColor = true;
+            this.CmdStop.Click += new System.EventHandler(this.CmdStop_Click);
+            // 
+            // TimKran
+            // 
+            this.TimKran.Interval = 200;
+            this.TimKran.Tick += new System.EventHandler(this.TimKran_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1600, 1000);
+            this.Controls.Add(this.CmdStop);
+            this.Controls.Add(this.CmdStart);
+            this.Controls.Add(this.rBtnKranEin);
+            this.Controls.Add(this.rBtnKranAus);
+            this.Controls.Add(this.rBtnKranLinks);
+            this.Controls.Add(this.rBtnKranRechts);
+            this.Controls.Add(this.rBtnAuslegerEin);
+            this.Controls.Add(this.rBtnAuslegerAus);
+            this.Controls.Add(this.rBtnHakenEin);
+            this.Controls.Add(this.rBtnHakenAus);
             this.Controls.Add(this.PnlAusleger);
             this.Controls.Add(this.PnlHauptelement);
             this.Controls.Add(this.PnlFundament);
@@ -187,6 +323,7 @@
             this.Name = "Form1";
             this.Text = "Kran";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -204,6 +341,17 @@
         private System.Windows.Forms.Panel PnlAusleger;
         private System.Windows.Forms.Panel PnlHauptelement;
         private System.Windows.Forms.Panel PnlFundament;
+        private System.Windows.Forms.RadioButton rBtnHakenAus;
+        private System.Windows.Forms.RadioButton rBtnHakenEin;
+        private System.Windows.Forms.RadioButton rBtnAuslegerAus;
+        private System.Windows.Forms.RadioButton rBtnAuslegerEin;
+        private System.Windows.Forms.RadioButton rBtnKranRechts;
+        private System.Windows.Forms.RadioButton rBtnKranLinks;
+        private System.Windows.Forms.RadioButton rBtnKranAus;
+        private System.Windows.Forms.RadioButton rBtnKranEin;
+        private System.Windows.Forms.Button CmdStart;
+        private System.Windows.Forms.Button CmdStop;
+        private System.Windows.Forms.Timer TimKran;
     }
 }
 
--- UKran/Form1.cs
+++ UKran/Form1.cs
@@ -20,13 +20,13 @@ namespace UKran
         private void CmdHakenAus_Click(object sender, EventArgs e)
         {
             int step;
-            if (PnlHaken.Location.Y + PnlHaken.Height <= ClientSize.Height -20)
+            if (PnlHaken.Location.Y + PnlHaken.Height <= ClientSize.Height - 20)
             {
                 step = 10;
             }
             else if (PnlHaken.Location.Y + PnlHaken.Height <= ClientSize.Height - 10)
             {
-                step = ClientSize.Height - (PnlHaken.Location.Y + PnlHaken.Height) -10;
+                step = ClientSize.Height - (PnlHaken.Location.Y + PnlHaken.Height) - 10;
             }
             else
             {
@@ -43,11 +43,11 @@ namespace UKran
         private void CmdAuslegerAus_Click(object sender, EventArgs e)
         {
             int step = 0;
-            if (PnlAusleger.Location.X >= 10 )
+            if (PnlAusleger.Location.X >= 10)
             {
                 step = 10;
             }
-            else if (PnlAusleger.Location.X >= 0 )
+            else if (PnlAusleger.Location.X >= 0)
             {
                 step = PnlAusleger.Location.X;
             }
@@ -62,21 +62,21 @@ namespace UKran
 
         private void CmdAuslegerEin_Click(object sender, EventArgs e)
         {
-            
+
             if (PnlAusleger.Width > 10)
             {
                 PnlAusleger.Width -= 10;
                 PnlAusleger.Location = new Point(PnlAusleger.Location.X + 10, PnlAusleger.Location.Y);
                 PnlHaken.Location = new Point(PnlHaken.Location.X + 10, PnlHaken.Location.Y);
             }
-            
+
         }
 
         private void CmdKranRechts_Click(object sender, EventArgs e)
         {
-            
+
             int step = 0;
-            if (PnlFundament.Location.X + PnlFundament.Width <= CmdAuslegerAus.Location.X -10)
+            if (PnlFundament.Location.X + PnlFundament.Width <= CmdAuslegerAus.Location.X - 10)
             {
                 step = 10;
             }
@@ -97,10 +97,10 @@ namespace UKran
         private void CmdKranLinks_Click(object sender, EventArgs e)
         {
             int step = 0;
-            if (PnlAusleger.Location.X >= 10  && PnlFundament.Location.X >= 10)
+            if (PnlAusleger.Location.X >= 10 && PnlFundament.Location.X >= 10)
             {
                 step = 10;
-            } 
+            }
             else if (PnlAusleger.Location.X >= 0 || PnlFundament.Location.X > 0)
             {
                 step = Math.Min(PnlAusleger.Location.X, PnlFundament.Location.X);
@@ -132,9 +132,9 @@ namespace UKran
                 return;
             }
 
-            PnlAusleger.Location = new Point(PnlAusleger.Location.X, PnlAusleger.Location.Y -step);
-            PnlHaken.Location = new Point(PnlHaken.Location.X, PnlHaken.Location.Y -step);
-            PnlHauptelement.Location = new Point(PnlHauptelement.Location.X, PnlHauptelement.Location.Y -step);
+            PnlAusleger.Location = new Point(PnlAusleger.Location.X, PnlAusleger.Location.Y - step);
+            PnlHaken.Location = new Point(PnlHaken.Location.X, PnlHaken.Location.Y - step);
+            PnlHauptelement.Location = new Point(PnlHauptelement.Location.X, PnlHauptelement.Location.Y - step);
             PnlHauptelement.Height += step;
         }
 
@@ -147,6 +147,58 @@ namespace UKran
                 PnlHauptelement.Location = new Point(PnlHauptelement.Location.X, PnlHauptelement.Location.Y + 10);
                 PnlHauptelement.Height -= 10;
             }
+        }
+
+        private void rBtnHakenAus_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdStart_Click(object sender, EventArgs e)
+        {
+            TimKran.Enabled = true;
+        }
+
+        private void CmdStop_Click(object sender, EventArgs e)
+        {
+            TimKran.Enabled = false;
+        }
+
+        private void TimKran_Tick(object sender, EventArgs e)
+        {
+            if (rBtnHakenAus.Checked)
+            {
+                CmdHakenAus_Click(sender, e);
+            }
+            else if (rBtnHakenEin.Checked)
+            {
+                CmdHakenEin_Click(sender, e);
+            }
+            else if (rBtnAuslegerAus.Checked)
+            {
+                CmdAuslegerAus_Click(sender, e);
+            }
+            else if (rBtnAuslegerEin.Checked)
+            {
+                CmdAuslegerEin_Click(sender, e);
+            }
+            else if (rBtnKranAus.Checked)
+            {
+                CmdKranAus_Click(sender, e);
+            }
+            else if (rBtnKranEin.Checked)
+            {
+                CmdKranEin_Click(sender, e);
+            }
+            else if (rBtnKranRechts.Checked)
+            {
+                CmdKranRechts_Click(sender, e);
+            }
+            else if (rBtnKranLinks.Checked)
+            {
+                CmdKranLinks_Click(sender, e);
+            }
+
         }
     }
 }
